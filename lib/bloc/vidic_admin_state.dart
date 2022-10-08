f2caac8a4a58392bee5b749ec4bc6630b3f3c19e
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-08 21:38:36 +0300

    Create vidic_admin_state.dart

---
+++ lib/bloc/vidic_admin_state.dart
@@ -0,0 +1,10 @@
+part of 'vidic_admin_bloc.dart';
+
+abstract class VidicAdminState extends Equatable {
+  const VidicAdminState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class VidicAdminInitial extends VidicAdminState {}
