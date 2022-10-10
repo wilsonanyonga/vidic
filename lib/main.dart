fc07978910c13837822ba49718d259662bb1c4c1
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-10 07:53:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,54 +33,87 @@ class MyApp extends StatelessWidget {
             create: (context) => DioClient(),
           ),
         ],
-        child: MaterialApp.router(
-          routeInformationProvider: _router.routeInformationProvider,
-          routeInformationParser: _router.routeInformationParser,
-          routerDelegate: _router.routerDelegate,
+        child: StreamBuilder<User?>(
+            stream: Auth().authStateChanges,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return MaterialApp.router(
+                  routeInformationProvider: _router.routeInformationProvider,
+                  routeInformationParser: _router.routeInformationParser,
+                  routerDelegate: _router.routerDelegate,
 
-          title: 'Flutter Demo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              // This is the theme of your application.
-              //
-              // Try running your application with "flutter run". You'll see the
-              // application has a blue toolbar. Then, without quitting the app, try
-              // changing the primarySwatch below to Colors.green and then invoke
-              // "hot reload" (press "r" in the console where you ran "flutter run",
-              // or simply save your changes to "hot reload" in a Flutter IDE).
-              // Notice that the counter didn't reset back to zero; the application
-              // is not restarted.
-              primarySwatch: Colors.blue,
-              scaffoldBackgroundColor: Colors.grey[350]),
-          // home: MyHomePage(title: 'Flutter Demo Home Page'),
-        ),
+                  title: 'Flutter Demo',
+                  debugShowCheckedModeBanner: false,
+                  theme: ThemeData(
+                      // This is the theme of your application.
+                      //
+                      // Try running your application with "flutter run". You'll see the
+                      // application has a blue toolbar. Then, without quitting the app, try
+                      // changing the primarySwatch below to Colors.green and then invoke
+                      // "hot reload" (press "r" in the console where you ran "flutter run",
+                      // or simply save your changes to "hot reload" in a Flutter IDE).
+                      // Notice that the counter didn't reset back to zero; the application
+                      // is not restarted.
+                      primarySwatch: Colors.blue,
+                      scaffoldBackgroundColor: Colors.grey[350]),
+                  // home: MyHomePage(title: 'Flutter Demo Home Page'),
+                );
+              } else {
+                return MaterialApp.router(
+                  routeInformationProvider:
+                      _routerLogin.routeInformationProvider,
+                  routeInformationParser: _routerLogin.routeInformationParser,
+                  routerDelegate: _routerLogin.routerDelegate,
+
+                  title: 'Flutter Demo',
+                  debugShowCheckedModeBanner: false,
+                  theme: ThemeData(
+                      // This is the theme of your application.
+                      //
+                      // Try running your application with "flutter run". You'll see the
+                      // application has a blue toolbar. Then, without quitting the app, try
+                      // changing the primarySwatch below to Colors.green and then invoke
+                      // "hot reload" (press "r" in the console where you ran "flutter run",
+                      // or simply save your changes to "hot reload" in a Flutter IDE).
+                      // Notice that the counter didn't reset back to zero; the application
+                      // is not restarted.
+                      primarySwatch: Colors.blue,
+                      scaffoldBackgroundColor: Colors.grey[350]),
+                  // home: MyHomePage(title: 'Flutter Demo Home Page'),
+                );
+              }
+            }),
       );
 
   final GoRouter _router = GoRouter(
     routes: <GoRoute>[
       GoRoute(
         path: '/',
-        redirect: (state) {
-          if (await Auth().authStateChanges.isEmpty) {
-            
-          } else {
-            
-          }
-          // return StreamBuilder(
-              // stream: Auth().authStateChanges,
-              // builder: (context, snapshot) {
-              //   if (snapshot.hasData) {
-              //     return MyHomePage(title: 'Flutter Demo Home Page');
-              //   } else {
-              //     return Login();
-              //   }
-              // },
-              // );
-          // return '/login';
-        },
-        // builder: (BuildContext context, GoRouterState state) =>
-        //     MyHomePage(title: 'Flutter Demo Home Page'),
-        // builder: (BuildContext context, GoRouterState state) => Login(),
+        // redirect: (state) {
+        //   if (await Auth().authStateChanges.isEmpty) {
+
+        //   } else {
+
+        //   }
+        //   // return StreamBuilder(
+        //       // stream: Auth().authStateChanges,
+        //       // builder: (context, snapshot) {
+        //       //   if (snapshot.hasData) {
+        //       //     return MyHomePage(title: 'Flutter Demo Home Page');
+        //       //   } else {
+        //       //     return Login();
+        //       //   }
+        //       // },
+        //       // );
+        //   // return '/login';
+        // },
+
+        builder: (BuildContext context, GoRouterState state) =>
+            MyHomePage(title: 'Flutter Demo Home Page'),
+        // // builder: (BuildContext context, GoRouterState state) => Login(),
+
+        // redirect: (state) => '/dala',
+
         routes: [
           GoRoute(
             path: 'login',
@@ -120,6 +154,18 @@ class MyApp extends StatelessWidget {
                 ComplaintsScreen(),
           ),
         ],
+      ),
+    ],
+  );
+
+  final GoRouter _routerLogin = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+
+        builder: (BuildContext context, GoRouterState state) => Login(),
+
+        // redirect: (state) => '/dala',
       ),
     ],
   );
