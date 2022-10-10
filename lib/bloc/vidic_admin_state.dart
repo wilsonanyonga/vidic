42c33e6718e53ce9621c59362f17a5d7138d917d
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-10 07:54:07 +0300

    Update vidic_admin_state.dart

--- lib/bloc/vidic_admin_state.dart
+++ lib/bloc/vidic_admin_state.dart
@@ -2,9 +2,11 @@ part of 'vidic_admin_bloc.dart';
 
 abstract class VidicAdminState extends Equatable {
   const VidicAdminState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class VidicAdminInitial extends VidicAdminState {}
+
+class Login extends VidicAdminState {}
