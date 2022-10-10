f3d8c45ec6a9920d7251b82577e97642af8b9acb
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-10 07:38:08 +0300

    Update menu_bar.dart

--- lib/widgets/menu_bar.dart
+++ lib/widgets/menu_bar.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:vidic/utils/auth.dart';
 
 class MenuBarWidget extends StatelessWidget {
   const MenuBarWidget({
     Key? key,
   }) : super(key: key);
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,38 +20,43 @@ class MenuBarWidget extends StatelessWidget {
             decoration: const BoxDecoration(
               color: Colors.white,
             ),
-            child: Stack(
-              children: [
-                const Positioned(
-                  right: 135,
-                  top: 5,
-                  child: CircleAvatar(
-                    radius: 15,
-                    foregroundImage: AssetImage('assets/icons/vidic.png'),
+            child: GestureDetector(
+              onTap: () {
+                signOut();
+              },
+              child: Stack(
+                children: [
+                  const Positioned(
+                    right: 135,
+                    top: 5,
+                    child: CircleAvatar(
+                      radius: 15,
+                      foregroundImage: AssetImage('assets/icons/vidic.png'),
+                    ),
                   ),
-                ),
-                Positioned(
-                  right: 10,
-                  top: 1,
-                  child: Row(
-                    children: const [
-                      Text(
-                        'Francis Otworo',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Icon(Icons.arrow_drop_down)
-                    ],
+                  Positioned(
+                    right: 10,
+                    top: 1,
+                    child: Row(
+                      children: const [
+                        Text(
+                          'Francis Otworo',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Icon(Icons.arrow_drop_down)
+                      ],
+                    ),
                   ),
-                ),
-                const Positioned(
-                  right: 101,
-                  top: 25,
-                  child: Text(
-                    'Admin',
-                    style: TextStyle(fontSize: 10),
+                  const Positioned(
+                    right: 101,
+                    top: 25,
+                    child: Text(
+                      'Admin',
+                      style: TextStyle(fontSize: 10),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
