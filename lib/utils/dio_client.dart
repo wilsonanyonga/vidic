ee7b5c007414b92662a713a8674a5c9f77f694c4
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-18 17:01:26 +0300

    Update dio_client.dart

--- lib/utils/dio_client.dart
+++ lib/utils/dio_client.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:vidic/models/jwt.dart';
+import 'package:vidic/utils/logging.dart';
 
 class DioClient {
   final Dio _dio = Dio(
@@ -8,5 +11,44 @@ class DioClient {
       connectTimeout: 5000,
       receiveTimeout: 3000,
     ),
-  );
+  )..interceptors.add(Logging());
+
+  Future<Jwt?> verifyNumber(number, amount) async {
+    Jwt? mpesaPay;
+
+    try {
+      Response userData = await _dio.post(
+        '/sendNumber',
+        data: {
+          "number": number.toString(),
+          "amount": amount.toString(),
+        },
+      );
+
+      if (kDebugMode) {
+        print('User Info: ${userData.toString()}');
+      }
+
+      mpesaPay = Jwt.fromJson(userData.data);
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        if (kDebugMode) {
+          print('Dio error!');
+          print('STATUS: ${e.response?.statusCode}');
+          print('DATA: ${e.response?.data}');
+          print('HEADERS: ${e.response?.headers}');
+        }
+      } else {
+        // Error due to setting up or sending the request
+        if (kDebugMode) {
+          print('Error sending request!');
+          print(e.message);
+        }
+      }
+    }
+
+    return mpesaPay;
+  }
 }
