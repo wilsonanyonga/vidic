8925c5e7f278cf3966a0b6e368ce47d3b683f150
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-18 17:01:17 +0300

    Create logging.dart

---
+++ lib/utils/logging.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+class Logging extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (kDebugMode) {
+      print('REQUEST[${options.method}] => PATH: ${options.path}');
+    }
+    return super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (kDebugMode) {
+      print(
+        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}',
+      );
+    }
+    return super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    if (kDebugMode) {
+      print(
+        'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
+      );
+    }
+    return super.onError(err, handler);
+  }
+}
