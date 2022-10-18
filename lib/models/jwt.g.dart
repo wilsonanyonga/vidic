93e1d806e6b44414ad6ec9297be22167a1e4a637
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-18 17:01:34 +0300

    Create jwt.g.dart

---
+++ lib/models/jwt.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'jwt.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Jwt _$JwtFromJson(Map<String, dynamic> json) => Jwt(
+      data: json['data'] as String,
+      status: json['status'] as int,
+    );
+
+Map<String, dynamic> _$JwtToJson(Jwt instance) => <String, dynamic>{
+      'data': instance.data,
+      'status': instance.status,
+    };
