24212aadf41498d199021c53abab4d47749919de
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-19 21:12:31 +0300

    Update dio_client.dart

--- lib/utils/dio_client.dart
+++ lib/utils/dio_client.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:vidic/models/jwt.dart';
+import 'package:vidic/models/statement/get_statement.dart';
 import 'package:vidic/utils/logging.dart';
 
 class DioClient {
@@ -51,5 +52,38 @@ class DioClient {
     }
 
     return mpesaPay;
+  }
+
+  Future<GetStatement?> getStatement() async {
+    GetStatement? user;
+
+    try {
+      Response userData = await _dio.get('/listHome');
+
+      if (kDebugMode) {
+        print('User Info: ${userData.data}');
+      }
+
+      user = GetStatement.fromJson(userData.data);
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        if (kDebugMode) {
+          print('Dio error!');
+          print('STATUS: ${e.response?.statusCode}');
+          print('DATA: ${e.response?.data}');
+          print('HEADERS: ${e.response?.headers}');
+        }
+      } else {
+        // Error due to setting up or sending the request
+        if (kDebugMode) {
+          print('Error sending request!');
+          print(e.message);
+        }
+      }
+    }
+
+    return user;
   }
 }
