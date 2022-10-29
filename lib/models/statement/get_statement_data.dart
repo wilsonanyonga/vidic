9650bf6023632116ba9f7bbe8d4e9955baf2f8bc
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-29 11:12:54 +0300

    Update get_statement_data.dart

--- lib/models/statement/get_statement_data.dart
+++ lib/models/statement/get_statement_data.dart
@@ -5,45 +5,6 @@ part 'get_statement_data.g.dart';
 
 @JsonSerializable()
 class Datum {
-  Datum({
-    required this.id,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.deletedAt,
-    required this.datumId,
-    required this.name,
-    required this.number,
-    required this.vidicEmail,
-    required this.officialEmail,
-    required this.about,
-    required this.floor,
-    required this.rent,
-    required this.escalation,
-    required this.pobox,
-    required this.leaseStartDate,
-    required this.leaseEndDate,
-    required this.active,
-    required this.password,
-    required this.datumCreatedAt,
-    required this.datumUpdatedAt,
-    required this.statementTypes,
-    this.invoiceTypes,
-    this.lettersTypes,
-    this.lettersTenantTypes,
-    // this.id,
-    // this.createdAt,
-    // this.updatedAt,
-    // this.deletedAt,
-    // this.datumId,
-    // this.statementName,
-    // this.startDate,
-    // this.endDate,
-    // this.amount,
-    // this.tenantId,
-    // this.datumCreatedAt,
-    // this.datumUpdatedAt,
-  });
-
   @JsonKey(name: 'ID')
   int id;
   @JsonKey(name: 'CreatedAt')
@@ -51,7 +12,7 @@ class Datum {
   @JsonKey(name: 'UpdatedAt')
   DateTime updatedAt;
   @JsonKey(name: 'DeletedAt')
-  dynamic deletedAt;
+  DateTime? deletedAt;
   @JsonKey(name: 'id')
   int datumId;
   String name;
@@ -62,10 +23,13 @@ class Datum {
   String officialEmail;
   String about;
   String floor;
+  String size;
   String rent;
   String escalation;
   String pobox;
+  @JsonKey(name: 'lease_start_date')
   DateTime leaseStartDate;
+  @JsonKey(name: 'lease_end_date')
   DateTime leaseEndDate;
   String active;
   String password;
@@ -81,6 +45,47 @@ class Datum {
   dynamic lettersTypes;
   @JsonKey(name: 'LettersTenantTypes')
   dynamic lettersTenantTypes;
+
+  Datum({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.deletedAt,
+    required this.datumId,
+    required this.name,
+    required this.number,
+    required this.vidicEmail,
+    required this.officialEmail,
+    required this.about,
+    required this.floor,
+    required this.size,
+    required this.rent,
+    required this.escalation,
+    required this.pobox,
+    required this.leaseStartDate,
+    required this.leaseEndDate,
+    required this.active,
+    required this.password,
+    required this.datumCreatedAt,
+    required this.datumUpdatedAt,
+    required this.statementTypes,
+    required this.invoiceTypes,
+    required this.lettersTypes,
+    required this.lettersTenantTypes,
+    // this.id,
+    // this.createdAt,
+    // this.updatedAt,
+    // this.deletedAt,
+    // this.datumId,
+    // this.statementName,
+    // this.startDate,
+    // this.endDate,
+    // this.amount,
+    // this.tenantId,
+    // this.datumCreatedAt,
+    // this.datumUpdatedAt,
+  });
+
   // @JsonKey(name: 'ID')
   // int id;
   // @JsonKey(name: 'CreatedAt')
