0c8f6a8b2f363edf3ad02a361810ed6f47925bc3
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-30 21:08:44 +0300

    Create get_invoice.g.dart

---
+++ lib/models/invoice/get_invoice.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_invoice.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetInvoice _$GetInvoiceFromJson(Map<String, dynamic> json) => GetInvoice(
+      data: (json['data'] as List<dynamic>)
+          .map((e) => Datum.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      status: json['status'] as int,
+    );
+
+Map<String, dynamic> _$GetInvoiceToJson(GetInvoice instance) =>
+    <String, dynamic>{
+      'data': instance.data,
+      'status': instance.status,
+    };
