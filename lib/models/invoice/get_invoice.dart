207369eef7e1ed558faacd24218c54a6f251abc1
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-10-30 21:08:35 +0300

    Create get_invoice.dart

---
+++ lib/models/invoice/get_invoice.dart
@@ -0,0 +1,21 @@
+import 'package:vidic/models/invoice/get_invoice2.dart';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'get_invoice.g.dart';
+
+@JsonSerializable()
+class GetInvoice {
+  GetInvoice({
+    required this.data,
+    required this.status,
+  });
+
+  List<Datum> data;
+  int status;
+
+  factory GetInvoice.fromJson(Map<String, dynamic> json) =>
+      _$GetInvoiceFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetInvoiceToJson(this);
+}
