092aa4383b68deaa5bc1ce7d608fc18f77ae5ba0
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-11-03 11:14:27 +0300

    Update dio_client.dart

--- lib/utils/dio_client.dart
+++ lib/utils/dio_client.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:vidic/models/invoice/get_invoice.dart';
 import 'package:vidic/models/jwt.dart';
+import 'package:vidic/models/letter/get_letter.dart';
 import 'package:vidic/models/statement/get_statement.dart';
 import 'package:vidic/utils/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -162,6 +163,60 @@ class DioClient {
       }
 
       user = GetInvoice.fromJson(userData.data);
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        if (kDebugMode) {
+          print('Dio error!');
+          print('STATUS: ${e.response?.statusCode}');
+          print('DATA: ${e.response?.data}');
+          print('HEADERS: ${e.response?.headers}');
+        }
+      } else {
+        // Error due to setting up or sending the request
+        if (kDebugMode) {
+          print('Error sending request!');
+          print(e.message);
+        }
+      }
+    }
+
+    return user;
+  }
+
+  // get Letters
+  Future<GetLetter?> getLetter() async {
+    GetLetter? user;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+    stringValue = prefs.getString('jwt_token');
+    if (kDebugMode) {
+      print(stringValue);
+    }
+    try {
+      // _dio.options.headers[HttpHeaders.authorizationHeader] = "stringValue";
+      Response userData = await _dio.get(
+        '/getLetters',
+        options: Options(
+          headers: {
+            // "authorization": stringValue, // set content-length
+            "authorization": stringValue, // set content-length
+            // "Content-Type": "application/json",
+            // 'Accept': '*/*',
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,PATCH",
+            // "Content-Type": "application/json",
+          },
+        ),
+      );
+
+      if (kDebugMode) {
+        print('User Info: ${userData.data}');
+      }
+
+      user = GetLetter.fromJson(userData.data);
     } on DioError catch (e) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx and is also not 304.
