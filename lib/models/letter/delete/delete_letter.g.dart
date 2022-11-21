8d7a56e34d92a90fcdbc9ec077fed2690ae66cd0
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-11-21 16:32:13 +0300

    Create delete_letter.g.dart

---
+++ lib/models/letter/delete/delete_letter.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'delete_letter.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DeleteLetter _$DeleteLetterFromJson(Map<String, dynamic> json) => DeleteLetter(
+      data: json['data'] as bool,
+      status: json['status'] as int,
+    );
+
+Map<String, dynamic> _$DeleteLetterToJson(DeleteLetter instance) =>
+    <String, dynamic>{
+      'data': instance.data,
+      'status': instance.status,
+    };
