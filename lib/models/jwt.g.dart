5e99fa14b26a912cb3a7f461f032744293678d87
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-11-26 13:15:28 +0300

    Update jwt.g.dart

--- lib/models/jwt.g.dart
+++ lib/models/jwt.g.dart
@@ -9,9 +9,13 @@ part of 'jwt.dart';
 Jwt _$JwtFromJson(Map<String, dynamic> json) => Jwt(
       data: json['data'] as String,
       status: json['status'] as int,
+      user: (json['user'] as List<dynamic>)
+          .map((e) => UserJWT.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$JwtToJson(Jwt instance) => <String, dynamic>{
       'data': instance.data,
       'status': instance.status,
+      'user': instance.user,
     };
