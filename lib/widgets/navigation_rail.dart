cae3c89ee0d450b41c2f8f827f96aabf75ffffc0
Wilson Anyonga <wilsonanyonga@gmail.com>
2022-12-20 15:17:49 +0300

    Update navigation_rail.dart

--- lib/widgets/navigation_rail.dart
+++ lib/widgets/navigation_rail.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class WidgetNavigationRail extends StatelessWidget {
-  WidgetNavigationRail({
+  const WidgetNavigationRail({
     Key? key,
     required int selectedIndex,
   })  : _selectedIndex = selectedIndex,
